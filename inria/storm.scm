SadiinsoSnowfall/high-perf-clusterfuck

commit db92ddc10b246bf6235db353b7c0c0c280663927

More input label removal.

inria/storm.scm
@@ -101,8 +101,7 @@
            autoconf
            automake))                             ;used upon test failure
     (inputs (list fftw fftwf))
-    (propagated-inputs  `(("hwloc" ,hwloc-1 "lib")
-                          ("mpi" ,openmpi-with-mpi1-compat)))
+    (propagated-inputs  (list `(,hwloc-1 "lib") openmpi-with-mpi1-compat))
     (synopsis "Run-time system for heterogeneous computing")
     (description
      "StarPU is a run-time system that offers support for heterogeneous
@@ -181,11 +180,11 @@ kernels are executed as efficiently as possible.")
                             (substitute* "min-dgels/base/make.inc"
                               (("/bin/sh")  (which "sh")))
                             #t)))))))
-   (propagated-inputs  `(("mpi" ,openmpi)
-                         ("hwloc" ,hwloc "lib")  ;hwloc 2.x
-                         ,@(fold alist-delete
-                                 (package-propagated-inputs starpu-1.2)
-                                 '("mpi" "hwloc"))))))
+   (propagated-inputs  (modify-inputs (package-propagated-inputs starpu-1.2)
+                         (delete "mpi" "hwloc")
+                         (prepend openmpi
+                                  `(,hwloc "lib") ;hwloc 2.x
+                                  )))))
 
 ; next release of StarPU will have an optional dependency on tadaam/mpi_sync_clocks: don't forget to add it !
 
@@ -213,9 +212,8 @@ kernels are executed as efficiently as possible.")
      (substitute-keyword-arguments (package-arguments starpu)
        ((#:configure-flags flags '())
         `(cons "--enable-simgrid" (cons "--enable-mpi" (cons "--disable-shared" ,flags))))))
-    (inputs `(("simgrid" ,simgrid)
-              ("fxt" ,fxt+static)
-              ,@(package-inputs starpu)))
+    (inputs (modify-inputs (package-inputs starpu)
+              (prepend simgrid fxt+static)))
     (propagated-inputs `(,@(delete `("mpi" ,openmpi) (package-propagated-inputs starpu))))
     ;; some tests require python.
     (native-inputs

inria/bioversiton.scm
@@ -33,8 +33,7 @@
                                                              (chdir "src"))))
                         #:tests? #f))
 
-   (inputs `(("mpi" ,openmpi)
-             ("hdf5" , hdf5-parallel-openmpi)))
+   (inputs (list openmpi hdf5-parallel-openmpi))
 
    (synopsis "Compute pairwise distances between reads as edit distances")
    (description

inria/hiepacs.scm
@@ -812,25 +812,25 @@ etc.")
     (native-inputs
      (list pkg-config gfortran))
     (inputs
-     `(("gfortran:lib" ,gfortran "lib")           ;for 'gcc … -lgfortran'
-       ("openblas" ,openblas)
-       ;; ("lapack" ,lapack)         ;must be built with '-DLAPACKE_WITH_TMG=ON'
+     (list `(,gfortran "lib") ;for 'gcc … -lgfortran'
+           openblas
+           ;; ("lapack" ,lapack)         ;must be built with '-DLAPACKE_WITH_TMG=ON'
 
-       ;; Python bindings and Python tests. Python3
-       ("python" ,python)
+           ;; Python bindings and Python tests. Python3
+           python
 
-       ("python-numpy" ,python-numpy)
-       ;;("python-scipy" ,python-scipy)
-       ))
-    (propagated-inputs `(("hwloc" ,hwloc "lib")
-                         ("scotch" ,scotch)
-                         ;; The following are optional dependencies.
+           python-numpy
+           ;;("python-scipy" ,python-scipy)
+           ))
+    (propagated-inputs (list `(,hwloc "lib")
+                             scotch
 
-                         ;;GM: somehow these two are needed in propagated-inputs
-                         ;;in order to compile maphys++ (otherwise cmake fails
-                         ;;to find them)
-                         ("parsec" ,parsec+mpi)
-                         ("starpu" ,starpu)))
+                             ;; The following are optional dependencies.
+                             ;;GM: somehow these two are needed in propagated-inputs
+                             ;;in order to compile maphys++ (otherwise cmake fails
+                             ;;to find them)
+                             parsec+mpi
+                             starpu))
     (synopsis "Sparse matrix direct solver")
     (description
      "PaStiX (Parallel Sparse matriX package) is a scientific library that
@@ -904,20 +904,20 @@ memory footprint and/or the time-to-solution.")
     (native-inputs
      (list pkg-config gfortran))
     (inputs
-     `(("gfortran:lib" ,gfortran "lib")           ;for 'gcc … -lgfortran'
-       ("openblas" ,openblas)
-       ;; ("lapack" ,lapack)         ;must be built with '-DLAPACKE_WITH_TMG=ON'
+     (list `(,gfortran "lib") ;for 'gcc … -lgfortran'
+           openblas
+           ;; ("lapack" ,lapack)         ;must be built with '-DLAPACKE_WITH_TMG=ON'
 
-       ;; The following are optional dependencies.
-       ("parsec" ,parsec+mpi)
-       ("starpu" ,starpu)
+           ;; The following are optional dependencies.
+           parsec+mpi
+           starpu
 
-       ;; Python bindings and Python tests. Python3
-       ("python" ,python)
+           ;; Python bindings and Python tests. Python3
+           python
 
-       ("python-numpy" ,python-numpy)
-       ;;("python-scipy" ,python-scipy)
-       ))
+           python-numpy
+           ;;("python-scipy" ,python-scipy)
+           ))
     (propagated-inputs (list `(,hwloc "lib") scotch))
     (synopsis "Sparse matrix direct solver")
     (description
@@ -1230,15 +1230,12 @@ CTAGS    = $(CTAGSPROG)
                                (invoke "make" "examples")
                                (invoke "./example/bin/simple" "-lap" "100"))))))
   (inputs
-   `(("gfortran:lib" ,gfortran "lib")
-     ("openblas" ,openblas)))
+   (list `(,gfortran "lib") openblas))
   (native-inputs
    (list pkg-config gfortran perl))
   (propagated-inputs
-   `(("openmpi" ,openmpi-with-mpi1-compat)
-     ("hwloc" ,hwloc-1 "lib")
-     ("openssh" ,openssh)
-     ("scotch32" ,scotch32)))
+   (list openmpi-with-mpi1-compat
+         `(,hwloc-1 "lib") openssh scotch32))
   (outputs '( "out" "debug" ))
   (synopsis "Sparse matrix direct solver (version 5)")
   (description
@@ -1383,8 +1380,7 @@ to/from all other processes.")
     '(#:configure-flags '("-DBUILD_SHARED_LIBS=ON"
                           "-DICB=ON")))
    (inputs
-    `(("lapack" ,lapack)
-      ("fortran" ,gfortran)))
+    (list lapack gfortran))
    (synopsis "Fortran subroutines for solving eigenvalue problems")
    (description
     "ARPACK-NG is a collection of Fortran77 subroutines designed to solve
@@ -1457,8 +1453,7 @@ for manual interpretation.")
                         ))
    (build-system cmake-build-system)
    (inputs (list openblas fftw fftwf))
-   (propagated-inputs `(("mpi" ,openmpi)
-                        ("ssh" ,openssh)))
+   (propagated-inputs (list openmpi openssh))
    (native-inputs (list pkg-config))
    (properties '((tunable? . #true)))))
 

inria/mpi.scm
@@ -50,8 +50,7 @@
                                            ;; which is not allowed by default by OpenMPI
                                            (setenv "OMPI_MCA_rmaps_base_oversubscribe" "1") #t)))))
    (build-system cmake-build-system)
-   (propagated-inputs `(("mpi" ,openmpi)
-                        ("ssh" ,openssh)))))
+   (propagated-inputs (list openmpi openssh))))
 
 (define-public openmpi-curta
   ;; Open MPI package matching the version of Open MPI on the Curta

inria/simgrid.scm
@@ -39,11 +39,8 @@
        ;; and said logs include timestamps, pointer values, file names, etc.
        #:tests? #f))
     (native-inputs
-     `(("gfortran" ,gfortran)
-       ("gfortran:lib" ,gfortran "lib")
-       ("perl" ,perl)
-       ("python" ,python-wrapper)
-       ("doxygen" ,doxygen)))
+     (list gfortran
+           `(,gfortran "lib") perl python-wrapper doxygen))
     (propagated-inputs
      (list boost))
     ;; XXX: 'smpicc' and 'smpicxx' retain references to GCC.

inria/tadaam.scm
@@ -430,9 +430,7 @@ the MPI_THREAD_MULTIPLE multi-threading level.")
                  (delete 'check)))) ; no make check in mpibenchmark
    (native-inputs (list pkg-config autoconf automake))
    (inputs
-    `(("hwloc" ,hwloc "lib")
-      ("gnuplot" ,gnuplot)
-      ("openmpi" ,nmad)))
+    (list `(,hwloc "lib") gnuplot nmad))
    (synopsis "MPI overlap benchmark")
    (description "MadMPI benchmark contains the following benchmark series:
 - base send/recv benchmark, used for reference (mpi_bench_base);
@@ -474,7 +472,7 @@ are not benchmarked yet.")
    (native-inputs
     (list pkg-config autoconf automake))
    (inputs
-    `(("mpi" ,openmpi))) ; Every packet requiring mpi use openmpi, so use it, it will be simpler to then change with `--with-input=openmpi=nmad`
+    (list openmpi)) ; Every packet requiring mpi use openmpi, so use it, it will be simpler to then change with `--with-input=openmpi=nmad`
    (synopsis "Distributed synchronized clocks over MPI")
    (description "Small library with routines to synchronize clocks over several
                 nodes with MPI.")

Z572/guix-hpc

commit e57b7fc10b00a92c47e527e325df3bdfe862232b

starpu: Fix regression in MPI handling introduced by label removal.
This fixes a regression introduced in
db92ddc10b246bf6235db353b7c0c0c280663927, whereby we wouldn't be
removing the MPI implementation in some cases, and we wouldn't pass
"--enable-nmad" when nmad is used.

* inria/storm.scm (starpu-configure-flags): Use 'lookup-package-input'
and related procedures instead of 'assoc'.
(starpu-1.3)[propagated-inputs]: Delete "openmpi-mpi1-compat".
(starpu+simgrid)[propagated-inputs]: Delete "openmpi".

inria/storm.scm
@@ -1,7 +1,7 @@
 ;;; This module extends GNU Guix and is licensed under the same terms, those
 ;;; of the GNU GPL version 3 or (at your option) any later version.
 ;;;
-;;; Copyright © 2017-2022 Inria
+;;; Copyright © 2017-2023 Inria
 
 (define-module (inria storm)
   #:use-module (guix)
@@ -133,10 +133,10 @@ kernels are executed as efficiently as possible.")
 (define (starpu-configure-flags package)
   ;; Return the standard configure flags for PACKAGE, a StarPU 1.3+ package.
   `(list "--enable-quick-check"
-         ,@(if (assoc "fxt" (package-inputs package)) ;optional fxt dependency
+         ,@(if (lookup-package-input package "fxt") ;optional fxt dependency
                '("--with-fxt")
                '())
-         ,@(if (assoc "simgrid" (package-inputs package)) ;optional simgrid
+         ,@(if (lookup-package-input package "simgrid") ;optional simgrid
                '("--enable-simgrid"
                  "--enable-maxcpus=1000"
                  "--enable-maxcudadev=1000"
@@ -148,13 +148,9 @@ kernels are executed as efficiently as possible.")
                ;; the future, it would be nice to give the opportunity to
                ;; change it at will when parametrized packages will be there.
                '("--enable-maxcpus=128"))
-         ,@(match (assoc "mpi" (package-propagated-inputs package))
-             (("mpi" mpi)
-              (if (string=? (package-name mpi) "nmad")
-                  '("--enable-nmad")
-                  '()))
-             (#f
-              '()))))
+         ,@(if (lookup-package-propagated-input package "nmad")
+               '("--enable-nmad")
+               '())))
 
 (define-public starpu-1.3
   (package
@@ -181,7 +177,7 @@ kernels are executed as efficiently as possible.")
                               (("/bin/sh")  (which "sh")))
                             #t)))))))
    (propagated-inputs  (modify-inputs (package-propagated-inputs starpu-1.2)
-                         (delete "mpi" "hwloc")
+                         (delete "openmpi-mpi1-compat" "hwloc")
                          (prepend openmpi
                                   `(,hwloc "lib") ;hwloc 2.x
                                   )))))
@@ -214,7 +210,9 @@ kernels are executed as efficiently as possible.")
         `(cons "--enable-simgrid" (cons "--enable-mpi" (cons "--disable-shared" ,flags))))))
     (inputs (modify-inputs (package-inputs starpu)
               (prepend simgrid fxt+static)))
-    (propagated-inputs `(,@(delete `("mpi" ,openmpi) (package-propagated-inputs starpu))))
+    (propagated-inputs
+     (modify-inputs (package-propagated-inputs starpu)
+       (delete "openmpi")))
     ;; some tests require python.
     (native-inputs
      (modify-inputs (package-native-inputs starpu)