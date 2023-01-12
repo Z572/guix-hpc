2e24d592b7162e6b0329c818c15b2adccb3025ed
Ludovic Courtès <ludovic.courtes@inria.fr>
2023-01-12 15:02:22 +0100

    starpu-simgrid: Skip faulty test.
    Thanks to Samuel Thibault.
    
    * inria/storm.scm (starpu+simgrid)[arguments]: Add 'skip-faulty-test'
    phase.

--- inria/storm.scm
+++ inria/storm.scm
@@ -207,7 +207,14 @@ kernels are executed as efficiently as possible.")
     (arguments
      (substitute-keyword-arguments (package-arguments starpu)
        ((#:configure-flags flags '())
-        `(cons "--enable-simgrid" (cons "--enable-mpi" (cons "--disable-shared" ,flags))))))
+        `(cons "--enable-simgrid" (cons "--enable-mpi" (cons "--disable-shared" ,flags))))
+       ((#:phases phases '%standard-phases)
+        `(modify-phases ,phases
+           (add-before 'check 'skip-faulty-test
+             (lambda _
+               ;; This test fails in 1.3.10; fixed in StarPU commit
+               ;; 99d29fb1777b0833e97ce422f34cb88224d34f09.
+               (setenv "XFAIL_TESTS" "scheduler/schedulers.sh")))))))
     (inputs (modify-inputs (package-inputs starpu)
               (prepend simgrid fxt+static)))
     (propagated-inputs
