74005a1b7ee471300a83cdbff13ec8bdb37f3e4f
Ludovic Courtès <ludovic.courtes@inria.fr>
2023-05-02 12:15:55 +0200

    Remove 'maphys++-eigen'.
    This variant never built.
    
    * inria/hiepacs.scm (maphys++-eigen): Remove.

--- inria/hiepacs.scm
+++ inria/hiepacs.scm
@@ -698,22 +698,6 @@ is implemented in MPI.")
                    (inputs `(("librsb" ,librsb)
                              ,@(package-inputs maphys++)))))
 
-(define-public maphys++-eigen
-  ;; Variant of Maphys++ that uses Eigen instead of blaspp/lapackpp.
-  ;; FIXME: Currently fails to build (blaspp is required at configure time).
-  (package/inherit maphys++
-    (name "maphys++-eigen")
-    (arguments
-     (substitute-keyword-arguments (package-arguments maphys++)
-       ((#:configure-flags flags)
-        ''("-DMAPHYSPP_USE_EIGEN=ON"
-           "-DMAPHYSPP_USE_FABULOUS=ON"
-           "-DMAPHYSPP_USE_PADDLE=ON"))))
-    (inputs
-     `(("eigen" ,eigen)
-       ,@(fold alist-delete (package-inputs maphys++)
-               '("blaspp" "lapackpp"))))))
-
 (define-public blaspp
   (package
     (name "blaspp")
