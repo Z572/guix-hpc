e841345180cc3019df5170e8e829af1f8c662ce7
Ludovic Courtès <ludovic.courtes@inria.fr>
2023-06-16 10:03:30 +0200

    bitpit: Use 'git-fetch'.
    * inria/memphis.scm (bitpit): Use 'git-fetch'.

--- inria/memphis.scm
+++ inria/memphis.scm
@@ -1,10 +1,11 @@
 ;;; This module extends GNU Guix and is licensed under the same terms, those
 ;;; of the GNU GPL version 3 or (at your option) any later version.
 ;;;
-;;; Copyright © 2019, 2020 Inria
+;;; Copyright © 2019, 2020, 2023 Inria
 
 (define-module (inria memphis)
   #:use-module (guix)
+  #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build-system cmake)
   #:use-module (gnu packages)
@@ -14,38 +15,38 @@
   #:use-module (gnu packages mpi)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages ssh)
-  #:use-module (gnu packages xml)
-  )
+  #:use-module (gnu packages xml))
 
 (define-public bitpit
   (package
-   (name "bitpit")
-   (version "1.7.1")
-   (source
-    (origin
-     (method url-fetch)
-     (uri
-      "https://github.com/optimad/bitpit/archive/refs/tags/bitpit-1.7.1.tar.gz")
-     (sha256
-      (base32
-       "04438fd94a6px6al0cqcfas5qcaqbpsiic8h2vzn3jgbcm384vr0"))))
-   (inputs
-    (list libxml2 openblas))
-   (propagated-inputs
-    (list openmpi petsc-openmpi))
-   (native-inputs
-    (list pkg-config gfortran openssh))
-   (build-system cmake-build-system)
-   (arguments
-    '(#:configure-flags `("-DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON"
-                          "-DBUILD_SHARED_LIBS=ON"
-                          "-DENABLE_MPI=ON"
-                          ,(string-append "-DPETSC_DIR="
-                                          (assoc-ref %build-inputs "petsc-openmpi")))
-      #:tests? #f))
-   (home-page "http://optimad.github.io/bitpit/")
-   (synopsis
-    "C++ parallel library for structured and unstructured mesh handling.")
-   (description
-    "C++ parallel library for structured and unstructured mesh handling.")
-   (license license:lgpl3)))
+    (name "bitpit")
+    (version "1.7.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/optimad/bitpit")
+                    (commit (string-append "bitpit-" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0vc5irb9p4792wnldbl0pz6j852fvscqq17szzhzd6xrkvn8k2ii"))))
+    (inputs
+     (list libxml2 openblas))
+    (propagated-inputs
+     (list openmpi petsc-openmpi))
+    (native-inputs
+     (list pkg-config gfortran openssh))
+    (build-system cmake-build-system)
+    (arguments
+     '(#:configure-flags `("-DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON"
+                           "-DBUILD_SHARED_LIBS=ON"
+                           "-DENABLE_MPI=ON"
+                           ,(string-append "-DPETSC_DIR="
+                                           (assoc-ref %build-inputs "petsc-openmpi")))
+       #:tests? #f))
+    (home-page "http://optimad.github.io/bitpit/")
+    (synopsis
+     "C++ parallel library for structured and unstructured mesh handling.")
+    (description
+     "C++ parallel library for structured and unstructured mesh handling.")
+    (license license:lgpl3)))
