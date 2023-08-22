0e14f956e08f08df51229decdbcf69668348aa5a
Marc Fuentes <marc.fuentes@inria.fr>
2023-08-22 17:06:20 +0200

    Add blt.
    * llnl/geos.scm (blt): New variable.
    
    Co-authored-by: Ludovic Courtès <ludovic.courtes@inria.fr>

--- llnl/geos.scm
+++ llnl/geos.scm
@@ -31,3 +31,26 @@ projects. It's in the direction of projects like metal (a major influence)
 but with a focus on wide compiler compatibility across HPC-oriented systems.")
     (license license:bsd-3)))
 
+(define-public blt
+  (let ((commit "6a73c2cc3ad8147d2686c4eaa79beb43e77e1724")
+        (revision "20230605"))
+    (package
+      (name "blt")
+      (version (git-version "0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/LLNL/blt")
+                      (commit commit)))
+                (sha256
+                 (base32
+                  "1jiygcb2wzxxf82x95xcs8469z3ik0y7d14dblp1zaxq8vyz6x7p"))))
+      (build-system copy-build-system)
+      (arguments
+       '(#:install-plan '(("." "blt_dir"))))
+      (home-page "https://llnl-blt.readthedocs.io/en/develop/")
+      (synopsis "CMake macros and tools assembled to simplify HPC software development")
+      (description
+       "BLT is a streamlined CMake-based foundation for Building, Linking and
+Testing large-scale high performance computing (HPC) application.")
+      (license license:bsd-3))))
