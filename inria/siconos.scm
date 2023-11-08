f6d6373f6a4542585397178c2ec3e58c4990f0da
Maurice Bremond <Maurice.Bremond@inria.fr>
2023-11-08 12:10:47 +0100

    [siconos] fix regexp / patch cmake issue

--- inria/siconos.scm
+++ inria/siconos.scm
@@ -432,10 +432,9 @@ Mechanics, and Computer Graphics.")
                      (substitute* "cmake/SiconosSetup.cmake"
                                   (("FATAL_ERROR") "WARNING"))
                      (substitute* "cmake/fclib_setup.cmake"
-                                  (("find_package(FCLIB 3.0.0 CONFIG REQUIRED)")
+                                  (("find_package\\(FCLIB 3.0.0 CONFIG REQUIRED\\)")
                                    "find_package(FCLIB 3.0.0 CONFIG REQUIRED)
-                                    set(ConfigPackageLocation lib/cmake/siconos-${SICONOS_VERSION})"))
-
+    set(ConfigPackageLocation lib/cmake/siconos-${SICONOS_VERSION})"))
                      #t))))))))
 
 (define-public siconos siconos-4.4-rc3)
