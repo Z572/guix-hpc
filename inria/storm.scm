fc4709144aa2b9026da76fa909d390ac86ca3353
Ludovic Courtès <ludovic.courtes@inria.fr>
2023-12-15 11:24:05 +0100

    starpu: Really pass ‘--enable-nmad’ when using nmad.
    * inria/storm.scm (starpu-configure-flags): Tweak nmad detection.

--- inria/storm.scm
+++ inria/storm.scm
@@ -150,7 +150,15 @@ kernels are executed as efficiently as possible.")
                ;; the future, it would be nice to give the opportunity to
                ;; change it at will when parametrized packages will be there.
                '("--enable-maxcpus=128"))
-         ,@(if (lookup-package-propagated-input package "nmad")
+
+         ;; XXX: When using '--with-input=openmpi=nmad', the resulting input
+         ;; label remains "openmpi" so we cannot really look for "nmad",
+         ;; hence the extra 'lookup-package-propagated-input' call.
+         ,@(if (and=> (or (lookup-package-propagated-input package "nmad")
+                          (lookup-package-propagated-input package
+                                                           "openmpi"))
+                      (lambda (package)
+                        (string=? "nmad" (package-name package))))
                '("--enable-nmad")
                '())))
 
