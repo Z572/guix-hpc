47f9bb9dd6297f420217dcbdcba48be729493b3c
Romain GARBAGE <romain.garbage@inria.fr>
2024-02-19 11:18:06 +0100

    guix-hpc: Add Gysela module and package.
    * guix-hpc/packages/gysela.scm (gysela): New variable.

---
+++ guix-hpc/packages/gysela.scm
@@ -0,0 +1,97 @@
+;;; This module extends GNU Guix and is licensed under the same terms, those
+;;; of the GNU GPL version 3 or (at your option) any later version.
+;;;
+;;; Copyright © 2024 Inria
+
+(define-module (guix-hpc packages gysela)
+  #:use-module ((guix licenses)
+                #:prefix license:)
+  #:use-module (guix gexp)
+  #:use-module (guix utils)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages algebra)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages benchmark)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages documentation)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages code)
+  #:use-module (gnu packages cpp)
+  #:use-module (gnu packages documentation)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-science)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages logging)
+  #:use-module (gnu packages mpi)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages serialization)
+  #:use-module (gnu packages tls)
+  #:use-module (guix-hpc packages pdi)
+  #:use-module (guix-hpc packages utils))
+
+(define-public gyselalibxx
+  (let ((commit "a3be632c27742dea183bd20b59484f0599b87d39")
+        (version "0.1")
+        (revision "1"))
+    (package
+      (name "gyselalibxx")
+      (version (git-version version revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/gyselax/gyselalibxx")
+               (commit commit)
+               (recursive? #t))) ;TODO: unvendor dependencies in the CMakeList.txt file
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "15v5fhj3d3vj43lb485mqaxf2knh80pmxxc341c1d0la89mljy88"))))
+      (build-system cmake-build-system)
+      (inputs (list eigen
+                    fftw
+                    fftwf
+                    ginkgo
+                    googletest
+                    hdf5
+                    kokkos
+                    libyaml
+                    mdspan
+                    openmpi
+                    openblas
+                    paraconf))
+      (propagated-inputs (list pdi ;needed to set up PDI_PLUGIN_PATH
+                               pdiplugin-decl-hdf5-parallel
+                               pdiplugin-set-value
+                               pdiplugin-mpi
+                               ;; needed to set up GUIX_PYTHONPATH
+                               python
+                               python-dask
+                               python-h5py
+                               python-matplotlib
+                               python-numpy
+                               python-scipy
+                               python-sympy
+                               python-xarray
+                               python-pyyaml))
+      (native-inputs (list pkg-config))
+      (arguments
+       (list
+        #:configure-flags #~(list
+                             "-DGYSELALIBXX_DEPENDENCY_POLICIES=INSTALLED")
+        #:phases #~(modify-phases %standard-phases
+                     (add-before 'check 'mpi-setup
+                       #$%openmpi-setup))))
+      (synopsis
+       "Collection of C++ components for writing gyrokinetic semi-lagrangian codes")
+      (description
+       "Gyselalib++ is a collection of C++ components for writing gyrokinetic
+semi-lagrangian codes and similar as well as a collection of such
+codes.")
+      (home-page "https://gyselax.github.io/")
+      (license license:bsd-2))))
