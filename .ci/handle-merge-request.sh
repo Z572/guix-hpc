44287b9ec8ffa78dac970036a0984f46de8153af
Romain GARBAGE <romain.garbage@inria.fr>
2024-07-04 16:48:45 +0200

    Fix handle-merge-request.sh script.

--- .ci/handle-merge-request.sh
+++ .ci/handle-merge-request.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 export TOKEN=$1
 export CI_MERGE_REQUEST_PROJECT_ID=$2
 export CI_MERGE_REQUEST_IID=$3
@@ -16,10 +18,24 @@ send_gitlab_comment() {
 
 send_gitlab_comment "Starting new evaluation at [$URL]($URL)."
 
+echo "Waiting for the jobset to be evaluated..."
+while test $(curl "https://guix.bordeaux.inria.fr/api/evaluation?id=$ID" | jq ".status") -eq -1 ; do
+    sleep 120
+done
+
+# Exit if the evaluation failed
+if test $(curl "https://guix.bordeaux.inria.fr/api/evaluation?id=$ID" | jq ".status") -ne 0 ; then
+    echo "Evaluation failed"
+    send_gitlab_comment "Evaluation $ID failed."
+    exit 1
+fi
+
+echo "Waiting for the packages to be built..."
 while test $(curl "https://guix.bordeaux.inria.fr/api/latestbuilds?evaluation=$ID&nr=$NR" | jq "map(select(.finished == 0)) | length") -ne 0 ; do
     sleep 120
 done
 
+echo "Finished building packages, if any."
 export JSON=$(curl "https://guix.bordeaux.inria.fr/api/latestbuilds?evaluation=$ID&nr=$NR")
 export STATUS=$(echo $JSON | jq "map(select(.buildstatus != 0)) | length")
 export NBUILDS=$(echo $JSON | jq "length")
@@ -28,11 +44,11 @@ export SUCCEEDED="${SUCCEEDED:-None}"
 export FAILED=$(echo $JSON | jq "map(select(.buildstatus != 0) | .nixname) | join(\", \")" | sed -e 's/"//g')
 export FAILED="${FAILED:-None}"
 
-send_gitlab_comment "*Number of packages rebuilt*: $NBUILDS."
+send_gitlab_comment "*Number of packages rebuilt* (eval $ID): $NBUILDS packages rebuilt."
 
 if [ $NBUILDS -ne 0 ] ; then
-    send_gitlab_comment "*Succeeded builds*: $SUCCEEDED."
-    send_gitlab_comment "*Failed builds*: $FAILED."
+    send_gitlab_comment "*Succeeded builds* (eval $ID): $SUCCEEDED."
+    send_gitlab_comment "*Failed builds* (eval $ID): $FAILED."
 fi
 
 exit $STATUS
