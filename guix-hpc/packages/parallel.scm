3834a6733779343b8a21e2226e18b0c42485ef24
Romain GARBAGE <romain.garbage@inria.fr>
2024-07-11 09:36:40 +0200

    guix-hpc: Add prrte.
    * guix-hpc/packages/parallel.scm (prrte): New variable.

--- guix-hpc/packages/parallel.scm
+++ guix-hpc/packages/parallel.scm
@@ -18,6 +18,38 @@
   #:use-module (gnu packages python)
   #:use-module (guix-hpc packages mpi))
 
+(define-public prrte
+  (package
+   (name "prrte")
+   (version "3.0.6")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append
+                  "https://github.com/openpmix/prrte/releases/download/v"
+                  version "/prrte-" version ".tar.bz2"))
+            (sha256
+             (base32
+              "0wiy0vk37v4db1jgxza8bci0cczcvj34dalzsrlz05dk45zb7dl3"))))
+   (build-system gnu-build-system)
+   (arguments
+    (list #:configure-flags #~(list (string-append "--with-hwloc="
+                                                   (assoc-ref %build-inputs "hwloc"))
+                                    (string-append "--with-pmix=" #$(this-package-input "openpmix")))))
+   (inputs (list libevent
+                 `(,hwloc-2 "lib")
+                 openpmix))
+   (native-inputs (list perl))
+   (synopsis "PMIx Reference RunTime Environment (PRRTE)")
+   (description
+    "The PMIx Reference RunTime Environment is a runtime environment
+containing the reference implementation and capable of operating
+within a host SMS. The reference RTE therefore provides an easy way of
+exploring PMIx capabilities and testing PMIx-based applications
+outside of a PMIx-enabled environment.")
+   (home-page "https://openpmix.github.io/")
+   ;; The provided license is kind of BSD-style but specific.
+   (license (license:fsf-free "https://github.com/openpmix/prrte?tab=License-1-ov-file#License-1-ov-file"))))
+
 (define-public openpmix
   (package
    (name "openpmix")
