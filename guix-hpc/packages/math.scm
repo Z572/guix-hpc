462c729920bd691e0e762be2326efc30a28b7672
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-08-05 15:30:42 +0200

    bigdft: init at 1.9.5

---
+++ guix-hpc/packages/math.scm
@@ -0,0 +1,80 @@
+(define-module (guix-hpc packages math)
+  #:use-module (guix)
+  #:use-module ((guix licenses)
+                #:prefix license:)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages documentation)
+  #:use-module (gnu packages gcc)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages mpi)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-science)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages swig)
+  #:use-module (guix build utils)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix git-download)
+  #:use-module (guix packages))
+
+(define-public libxc
+  (package
+    (name "libxc")
+    (version "4.3.4")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://gitlab.com/libxc/libxc")
+             (commit version)))
+       (sha256
+        (base32 "03vnmh9ygx8rg7nm1wfgkx15yzjxzccgjvjzvai5y00hwmybk5y7"))))
+    (build-system cmake-build-system)
+    (native-inputs (list python gfortran perl))
+    (arguments
+     `(#:configure-flags '("-DBUILD_SHARED_LIBS=ON" "-DENABLE_FORTRAN=ON")))
+    (synopsis
+     "Library of exchange-correlation functionals for density-functional theory.")
+    (description
+     "Library of exchange-correlation functionals for
+density-functional theory. The aim is to provide a portable, well
+tested and reliable set of exchange and correlation functionals that
+can be used by a variety of programs.")
+    (home-page "https://libxc.gitlab.io")
+    (license license:mpl2.0)))
+
+(define-public ntpoly
+  (package
+    (name "ntpoly")
+    (version "3.1.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/william-dawson/NTPoly")
+             (commit (string-append "ntpoly-v" version))))
+       (sha256
+        (base32 "1pjc1hhawxpfg08xxbbbhd5a32rx6cyczilmi9g3wlilfrzxq6xp"))))
+    (build-system cmake-build-system)
+    (native-inputs (list gfortran
+                         swig
+                         python
+                         ;; tests
+                         python-mpi4py
+                         python-numpy
+                         python-scipy
+                         python-pyyaml
+                         openssh))
+
+    (inputs (list openmpi lapack))
+    (arguments
+     `(#:configure-flags (list "-DBUILD_SHARED_LIBS=ON")))
+    (synopsis
+     "Massively parallel library for computing the functions of sparse matrices")
+    (description
+     "Massively parallel library for computing the functions of 
+sparse, Hermitian matrices based on polynomial expansions. For sufficiently 
+sparse matrices, most of the matrix functions in NTPoly can be computed in linear time.")
+    (home-page "https://william-dawson.github.io/NTPoly")
+    (license license:expat)))
