6863310414e243d1bce3b1e798fe3c10784d0e9a
Philippe SWARTVAGHER <philippe.swartvagher@inria.fr>
2024-08-05 12:18:42 +0200

    Small fixes for vite
    - use real website URL as homepage
    - fix release date showed in About window
    - build with OTF2

--- guix-hpc/packages/traces.scm
+++ guix-hpc/packages/traces.scm
@@ -10,9 +10,11 @@
   #:use-module (guix utils)
   #:use-module (guix build-system cmake)
   #:use-module (gnu packages)
-  #:use-module (gnu packages qt)
   #:use-module (gnu packages gl)
-  #:use-module (gnu packages maths))
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages profiling)
+  #:use-module (gnu packages qt)
+  #:use-module (gnu packages version-control))
 
 (define S specification->package)
 
@@ -20,36 +22,39 @@
   (package
     (name "vite")
     (version "1.4")
-    (home-page "https://gitlab.inria.fr/solverstack/vite/")
+    (home-page "https://solverstack.gitlabpages.inria.fr/vite/")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
-             (url home-page)
+             (url "https://gitlab.inria.fr/solverstack/vite.git")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
         (base32 "1f1a7p6mpjdw0ps0by1c0plxj2mnzsk59gya176yzk6bmh2khqyg"))))
     (build-system cmake-build-system)
     (arguments
-     '(#:configure-flags (list "-DVITE_ENABLE_MT_PARSERS=ON"
-
-                               ;; XXX: This requires a very old version of
-                               ;; Boost, older than 1.59.
-                               ;; "-DVITE_ENABLE_SERIALIZATION=TRUE"
-                               
-                               ;; TODO: Package OTF.
-                               ;; "-DVITE_ENABLE_OTF=TRUE"
-                               )
+     '(#:configure-flags (list "-DVITE_ENABLE_MT_PARSERS=ON" "-DVITE_ENABLE_OTF2=ON")
+       #:phases (modify-phases %standard-phases
+                               ;; when SOURCE_DATE_EPOCH is set, CMakeLists.txt
+                               ;; uses it as the date to display in the About
+                               ;; window, however, this phase sets it to 0.
+                               ;; When SOURCE_DATE_EPOCH is not set,
+                               ;; CMakeLists.txt will use the date of the Git
+                               ;; tag.
+                               (delete 'set-SOURCE-DATE-EPOCH))
        #:tests? #f))
     (inputs (list (S "qtbase@5")
                   mesa
                   glew
                   glu
                   glm
+                  `(,otf2 "lib")
                   (S "qtcharts@5")
                   (S "qtsvg@5")))
-    (native-inputs (list (S "qttools@5")))
+    (native-inputs (list (S "qttools@5")
+                   git  ;; used to get Git tag date and include it in about window
+                   ))
     (synopsis "Visualize program execution traces")
     (description
      "ViTE is a trace explorer.  It is a tool to visualize execution traces
