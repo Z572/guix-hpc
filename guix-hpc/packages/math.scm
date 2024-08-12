1ab4b8f82aa20183ab27f249123cd4c3f8e6e019
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-08-12 14:33:12 +0200

    freefrem: update to 4.14

--- guix-hpc/packages/math.scm
+++ guix-hpc/packages/math.scm
@@ -8,14 +8,12 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
-  #:use-module (gnu packages algebra)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages base)
   #:use-module (gnu packages bison)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages flex)
   #:use-module (gnu packages gcc)
-  #:use-module (gnu packages graphics)
   #:use-module (gnu packages maths)
   #:use-module (gnu packages mpi)
   #:use-module ((guix licenses)
@@ -24,7 +22,7 @@
 (define-public freefem
   (package
     (name "freefem")
-    (version "4.10")
+    (version "4.14")
     (source
      (origin
        (method git-fetch)
@@ -33,21 +31,10 @@
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1qsx3jvipnrsd6x7m38mnj6dixxsf70ar80b9gy4rnjrsbdf6iqh"))))
+        (base32 "0q1817slryrh8cvqhaj9gf6779wbah398a5ybrqgm4vlxx3zhj1j"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases (modify-phases %standard-phases
-                  (add-before 'check 'mpi-setup
-                    ;; Set the test environment for Open MPI.
-                    ,%openmpi-setup)
-                  (add-before 'configure 'set-mmg-path
-                    (lambda* (#:key inputs #:allow-other-keys)
-                      ;; <libmmg.h> is in the mmg/ subdirectory.  Extends the
-                      ;; header search path accordingly.
-                      (setenv "CPATH"
-                              (string-append (search-input-directory inputs
-                                              "/include/mmg") ":"
-                                             (getenv "CPATH")))))
                   (add-before 'check 'skip-faulty-tests
                     (lambda _
                       ;; XXX: Fix failing tests.
@@ -56,7 +43,9 @@
                          "")) ;ARPACK-related
                       (substitute* "examples/3dSurf/Makefile"
                         (("Pinocchio\\.edp")
-                         ""))))))) ;MMG-related
+                         "") ;MMG-related
+                        (("testvtk\\.edp")
+                         "")))))))
     (native-inputs (list autoconf
                          automake
                          unzip
@@ -64,23 +53,10 @@
                          bison
                          flex
                          gfortran))
-    (inputs (list ;petsc-openmpi
-                  gsl
-                  ipopt
-                  nlopt
-                  mumps ;FIXME: ./configure fails to use mumps
-                  (list mmg "lib")
+    (inputs (list ;-
                   suitesparse-umfpack
                   suitesparse-config
-                  suitesparse-amd
                   suitesparse-cholmod
-                  hdf5
-                  fftw
-                  arpack-ng
-                  scalapack
-                  scotch
-                  pt-scotch
-                  metis
                   openmpi
                   lapack))
     (properties `((tunable? . #t)))
