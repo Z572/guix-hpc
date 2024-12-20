4f770cc37a2ef70cafa8613b5eed250906711c5e
Romain GARBAGE <romain.garbage@inria.fr>
2024-12-20 12:07:56 +0100

    guix-hpc: benchmark: Add netlib-hpl.
    * guix-hpc/packages/benchmark.scm (netlib-hpl): New variable.

--- guix-hpc/packages/benchmark.scm
+++ guix-hpc/packages/benchmark.scm
@@ -11,7 +11,9 @@
   #:use-module (guix build utils)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system gnu)
+  #:use-module (gnu packages)
   #:use-module (gnu packages cpp)
+  #:use-module (gnu packages maths)
   #:use-module (gnu packages mpi)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages gcc)
@@ -265,3 +267,48 @@ as different classes.")
                           (mkdir-p (string-append #$output "/bin"))))))))
     (synopsis "NAS Parallel Benchmarks (NPB), MPI variant")))
 
+(define-public netlib-hpl
+  (package
+    (name "netlib-hpl")
+    (version "2.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://netlib.org/benchmark/hpl/hpl-" version
+                           ".tar.gz"))
+       (sha256
+        (base32 "0c18c7fzlqxifz1bf3izil0bczv3a7nsv0dn6winy3ik49yw3i9j"))
+       (patches (search-patches
+                 "guix-hpc/packages/patches/hpl-use-default-hpl-dat.patch"))))
+    (build-system gnu-build-system)
+    (inputs (list openblas openmpi))
+    (arguments
+     (list #:make-flags #~(list "arch=x86_64")
+           #:phases #~ (modify-phases %standard-phases
+                         (add-after 'unpack 'set-hpl-dat-path
+                           (lambda _
+                             (substitute* "testing/ptest/HPL_pdinfo.c"
+                               (("@@GUIX_HPL_DAT@@")
+                                (string-append #$output
+                                               "/share/"
+                                               #$(package-name this-package)
+                                               "-"
+                                               #$(package-version this-package)
+                                               "/examples")))))
+                         (add-after 'install 'install-hpl-dat
+                           (lambda _
+                             (install-file "testing/ptest/HPL.dat"
+                                           (string-append #$output
+                                                          "/share/"
+                                                          #$(package-name this-package)
+                                                          "-"
+                                                          #$(package-version this-package)
+                                                          "/examples")))))))
+    (home-page "https://netlib.org/benchmark/hpl/")
+    (synopsis "High-Performance Linpack Benchmark for Distributed-Memory Computers")
+    (description "HPL is a software package that solves a (random) dense linear system
+in double precision (64 bits) arithmetic on distributed-memory
+computers.  It can thus be regarded as a portable as well as freely
+available implementation of the High Performance Computing Linpack
+Benchmark.")
+    (license (license:non-copyleft "https://netlib.org/benchmark/hpl/copyright.html"))))
