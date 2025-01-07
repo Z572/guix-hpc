e271267ea93689a86a6729c24416cde0eac746b5
Emmanuel Agullo <emmanuel.agullo@inria.fr>
2025-01-07 12:05:06 +0100

    Fix: Move hwloc from propagated-inputs to inputs
    Following Romain's suggestion (thanks!)

--- inria/mpi.scm
+++ inria/mpi.scm
@@ -51,7 +51,8 @@
                                            ;; which is not allowed by default by OpenMPI
                                            (setenv "OMPI_MCA_rmaps_base_oversubscribe" "1") #t)))))
    (build-system cmake-build-system)
-   (propagated-inputs (list `(,hwloc "lib") openmpi))
+   (propagated-inputs (list openmpi))
+   (inputs (list `(,hwloc "lib")))
    (native-inputs (list pkg-config openssh))))
 
 (define-public openmpi-curta
