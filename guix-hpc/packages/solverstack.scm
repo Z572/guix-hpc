ae1bb4311fb34ed9a04c2c0719a26c4b564003f4
Florent Pruvost <florent.pruvost@inria.fr>
2025-01-16 15:48:59 +0100

    solverstack: Add a new package chameleon-aocl with aocl (i.e. amd) blas/lapack.

--- guix-hpc/packages/solverstack.scm
+++ guix-hpc/packages/solverstack.scm
@@ -394,6 +394,37 @@ area (CPUs-GPUs, distributed nodes).")
               (prepend starpu-hip)
               (delete "starpu")))))
 
+(define-public chameleon+aocl
+  (package/inherit chameleon
+    (name "chameleon-aocl")
+    (version "1.3.a76a80")
+    (home-page "https://gitlab.inria.fr/solverstack/chameleon")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url home-page)
+                    (commit "a76a8093aacf0c1abe2b9c07223e231c7ef78e6a")
+                    ;; We need the submodule in 'CMakeModules/morse_cmake'.
+                    (recursive? #t)))
+              (file-name (string-append name "-" version "-checkout"))
+              (sha256 (base32
+                       "0sch561c01zzp06x2r6ncya78rp91y0dxlpbsg3fzmh4yp6ql6jz"))
+              (modules '((guix build utils)))
+              ;; Do not install 'config.log' to avoid retaining a reference to GCC,
+              ;; GFortran, etc.
+              (snippet #~(substitute* "cmake_modules/PrintOpts.cmake"
+                           (("^INSTALL.*config\\.log.*" all)
+                            (string-append "# " all "\n"))))))
+    (arguments (substitute-keyword-arguments (package-arguments chameleon)
+                 ((#:configure-flags flags
+                   '())
+                  `(cons "-DLAPACK_LIBRARIES=-laoclutils -lflame -lblis"
+                         ,flags))))
+    (inputs (modify-inputs (package-inputs chameleon)
+              (prepend aocl-blis)
+              (prepend aocl-lapack)
+              (prepend aocl-utils)
+              (delete "openblas")))))
 
 (define-public mini-chameleon
   (package
